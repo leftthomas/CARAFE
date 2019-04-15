2e9483e8d4b28a1df950b828d82e4afb413b88fa
leftthomas <leftthomas@qq.com>
2019-04-15 21:24:14 +0800

    add model

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import math
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.utils import _pair
@@ -102,34 +103,57 @@ class WHConv(nn.Module):
 
 
 class Model(nn.Module):
+
     def __init__(self):
         super(Model, self).__init__()
-        self.conv1 = nn.Sequential(
-            WHConv(in_channels=9, out_channels=32, kernel_size=(1, 9)),
-            nn.MaxPool2d(kernel_size=(1, 2), stride=2)
-        )
-        self.conv2 = nn.Sequential(
-            HWConv(in_channels=32, out_channels=64, kernel_size=(1, 9)),
-            nn.MaxPool2d(kernel_size=(1, 2), stride=2)
-        )
-        self.fc1 = nn.Sequential(
-            nn.Linear(in_features=64 * 26, out_features=1000),
-            nn.ReLU()
-        )
-        self.fc2 = nn.Sequential(
-            nn.Linear(in_features=1000, out_features=500),
-            nn.ReLU()
-        )
-        self.fc3 = nn.Sequential(
-            nn.Linear(in_features=500, out_features=6)
-        )
 
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.conv2(out)
-        out = out.reshape(-1, 64 * 26)
-        out = self.fc1(out)
-        out = self.fc2(out)
-        out = self.fc3(out)
-        out = F.softmax(out, dim=1)
-        return out
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1)
+        torch.nn.init.xavier_uniform(self.conv1.weight)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        torch.nn.init.xavier_uniform(self.conv2.weight)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        torch.nn.init.xavier_uniform(self.conv3.weight)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.pool3 = nn.MaxPool2d(kernel_size=4)
+
+        self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1)
+        torch.nn.init.xavier_uniform(self.conv4.weight)
+        self.bn4 = nn.BatchNorm2d(512)
+        self.pool4 = nn.MaxPool2d(kernel_size=4)
+
+        self.fc1 = nn.Linear(in_features=2048, out_features=1024)
+        self.drop1 = nn.Dropout(0.5)
+
+        self.fc2 = nn.Linear(in_features=1024, out_features=256)
+        self.drop2 = nn.Dropout(0.5)
+
+        self.fc3 = nn.Linear(in_features=256, out_features=10)
+
+    def forward(self, inp):
+        x = F.relu(self.bn1(self.conv1(inp)))
+        x = self.pool1(x)
+
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool3(x)
+
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool4(x)
+
+        x = x.view(x.size()[0], -1)
+        x = F.relu(self.fc1(x))
+        x = self.drop1(x)
+
+        x = F.relu(self.fc2(x))
+        x = self.drop2(x)
+
+        x = F.log_softmax(self.fc3(x))
+        return x
