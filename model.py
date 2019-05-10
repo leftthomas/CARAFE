ab8ac0b01801d08b2203ac7fb7c6aa32bb2a4cfc
leftthomas <leftthomas@qq.com>
2019-05-10 19:34:39 +0800

    finish model

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import math
 
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.utils import _pair
@@ -107,53 +106,32 @@ class Model(nn.Module):
     def __init__(self, num_classes):
         super(Model, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1)
-        torch.nn.init.xavier_uniform_(self.conv1.weight)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=7, padding=3)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(kernel_size=5)
+        self.drop1 = nn.Dropout(0.3)
 
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-        torch.nn.init.xavier_uniform_(self.conv2.weight)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.pool2 = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=7, padding=3)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(kernel_size=(4, 100))
+        self.drop2 = nn.Dropout(0.3)
 
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
-        torch.nn.init.xavier_uniform_(self.conv3.weight)
-        self.bn3 = nn.BatchNorm2d(256)
-        self.pool3 = nn.MaxPool2d(kernel_size=4)
-
-        self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1)
-        torch.nn.init.xavier_uniform_(self.conv4.weight)
-        self.bn4 = nn.BatchNorm2d(512)
-        self.pool4 = nn.MaxPool2d(kernel_size=4)
-
-        self.fc1 = nn.Linear(in_features=2048, out_features=1024)
-        self.drop1 = nn.Dropout(0.5)
-
-        self.fc2 = nn.Linear(in_features=1024, out_features=256)
-        self.drop2 = nn.Dropout(0.5)
-
-        self.fc3 = nn.Linear(in_features=256, out_features=num_classes)
+        self.fc1 = nn.Linear(in_features=128, out_features=100)
+        self.drop3 = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(in_features=100, out_features=num_classes)
 
     def forward(self, inp):
         x = F.relu(self.bn1(self.conv1(inp)))
         x = self.pool1(x)
+        x = self.drop1(x)
 
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.pool2(x)
-
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = self.pool3(x)
-
-        x = F.relu(self.bn4(self.conv4(x)))
-        x = self.pool4(x)
+        x = self.drop2(x)
 
         x = x.view(x.size()[0], -1)
         x = F.relu(self.fc1(x))
-        x = self.drop1(x)
+        x = self.drop3(x)
 
-        x = F.relu(self.fc2(x))
-        x = self.drop2(x)
-
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
