c1c8ccd42a3e2a332cdee9756284b00e57e85e8f
leftthomas <leftthomas@qq.com>
2019-05-14 22:42:23 +0800

    use DataParallel

--- train.py
+++ train.py
@@ -35,16 +35,17 @@ transform_test = transforms.Compose([
 ])
 
 train_set = CIFAR10(root='data', train=True, download=True, transform=transform_train)
-train_loader = DataLoader(train_set, batch_size=32, shuffle=True, num_workers=4)
+train_loader = DataLoader(train_set, batch_size=128, shuffle=True, num_workers=4)
 
 test_set = CIFAR10(root='data', train=False, download=True, transform=transform_test)
-test_loader = DataLoader(test_set, batch_size=60, shuffle=False, num_workers=4)
+test_loader = DataLoader(test_set, batch_size=100, shuffle=False, num_workers=4)
 
 # Model
 print('==> Building model..')
 net = VGG()
 net = net.to(device)
 if device == 'cuda':
+    net = nn.DataParallel(net)
     cudnn.benchmark = True
 
 if args.resume:
