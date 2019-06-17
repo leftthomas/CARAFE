0a3efd1e11fe76f00948c9f5bb5827bae1d3f6b0
leftthomas <leftthomas@qq.com>
2019-06-17 19:43:50 +0800

    use capsule to replace linear

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 
 import torch.nn as nn
+from capsule_layer import CapsuleLinear
 from torchvision.models.resnet import ResNet, BasicBlock
 
 
@@ -17,11 +18,11 @@ class Model(nn.Module):
         self.features = nn.Sequential(*layers)
 
         # classifier
-        self.fc = nn.Linear(7 * 7 * 512, num_classes)
-        # self.fc = CapsuleLinear(out_capsules=16, in_length=64, out_length=32)
+        self.fc = CapsuleLinear(out_capsules=num_classes, in_length=512, out_length=64, in_capsules=49,
+                                share_weight=False, num_iterations=3, bias=False, return_prob=True)
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), -1)
-        out = self.fc(x)
+        x = x.view(x.size(0), -1, 512)
+        out, prob = self.fc(x)
         return out
