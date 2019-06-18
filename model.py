7f9030ef728fdea7c9480b6bee39e40e46011430
leftthomas <leftthomas@qq.com>
2019-06-18 17:25:01 +0800

    simplified code

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-
 import torch.nn as nn
 from capsule_layer import CapsuleLinear
 from torchvision.models.resnet import resnet18
@@ -15,16 +14,18 @@ class Model(nn.Module):
             if isinstance(module, nn.Linear) or isinstance(module, nn.AdaptiveAvgPool2d):
                 continue
             layers.append(module)
+        self.in_length, self.out_length = 64, 32
         self.features = nn.Sequential(*layers)
 
         # classifier
-        self.classifier = CapsuleLinear(out_capsules=num_classes, in_length=64, out_length=32, in_capsules=None,
-                                        share_weight=True, num_iterations=num_iterations, bias=False, return_prob=True)
+        self.classifier = CapsuleLinear(out_capsules=num_classes, in_length=self.in_length, out_length=self.out_length,
+                                        in_capsules=None, share_weight=True, num_iterations=num_iterations, bias=False,
+                                        return_prob=True)
 
     def forward(self, x):
         x = self.features(x)
         x = x.permute(0, 2, 3, 1).contiguous()
-        x = x.view(x.size(0), -1, 64)
+        x = x.view(x.size(0), -1, self.in_length)
         out, prob = self.classifier(x)
         out = out.norm(dim=-1)
         return out, prob
