87d88c4ce0d3e29d8354a31521db0193b16b3210
leftthomas <leftthomas@qq.com>
2019-08-25 15:46:01 +0800

    use new capsule layer api

--- model.py
+++ model.py
@@ -19,8 +19,7 @@ class Model(nn.Module):
         # classifier
         self.in_length, self.out_length = 256, 32
         self.classifier = CapsuleLinear(out_capsules=num_classes, in_length=self.in_length, out_length=self.out_length,
-                                        in_capsules=None, share_weight=True, num_iterations=num_iterations, bias=False,
-                                        return_prob=True)
+                                        in_capsules=361, share_weight=False, num_iterations=num_iterations)
 
     def forward(self, x):
         x = self.features(x)
