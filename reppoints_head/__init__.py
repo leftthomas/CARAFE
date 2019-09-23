a74b7061396cf989c89d46f357c633dc8336f41f
leftthomas <leftthomas@qq.com>
2019-09-24 02:01:24 +0800

    simplified code

--- reppoints_head/__init__.py
+++
@@ -1,8 +0,0 @@
-from .reppoints_head import RepPointsHead
-from .rpn_head import RPNHead
-
-__all__ = [
-    'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption', 'RPNHead',
-    'GARPNHead', 'RetinaHead', 'GARetinaHead', 'SSDHead', 'FCOSHead',
-    'RepPointsHead'
-]
