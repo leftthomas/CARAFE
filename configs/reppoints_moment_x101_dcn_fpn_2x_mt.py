42bbea6e99e9249d94bcf299a986a2011eb2c701
leftthomas <leftthomas@qq.com>
2019-09-28 15:36:24 +0800

    use muiti scale test

--- configs/reppoints_moment_x101_dcn_fpn_2x_mt.py
+++ configs/reppoints_moment_x101_dcn_fpn_2x_mt.py
@@ -94,7 +94,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2000, 1200),
+        img_scale=[(1666, 1000), (2000, 1200), (2333, 1400)],
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
