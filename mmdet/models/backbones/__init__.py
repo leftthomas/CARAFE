40e6a7992784fbd866c906766a7d20ad2734daf5
leftthomas <leftthomas@qq.com>
2019-10-01 16:43:13 +0800

    add res2net

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -2,5 +2,8 @@ from .hrnet import HRNet
 from .resnet import ResNet, make_res_layer
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
+from .res2net import Res2Net, res2net50, res2net50_14w_8s, res2net50_26w_4s, res2net50_26w_6s, res2net50_26w_8s, \
+    res2net50_48w_2s, res2net101_26w_4s
 
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet']
+__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net', 'res2net50', 'res2net50_14w_8s',
+           'res2net50_26w_4s', 'res2net50_26w_6s', 'res2net50_26w_8s', 'res2net50_48w_2s', 'res2net101_26w_4s']
