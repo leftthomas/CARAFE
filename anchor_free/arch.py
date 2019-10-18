5fb1a102379928c8d4ec8d07738594128dd430c3
leftthomas <leftthomas@qq.com>
2019-10-18 21:31:56 +0800

    finish arch

--- anchor_free/arch.py
+++ anchor_free/arch.py
@@ -1,6 +1,11 @@
+import logging
+
 import torch
 from detectron2.modeling.backbone import build_backbone
 from detectron2.modeling.meta_arch.build import META_ARCH_REGISTRY
+from detectron2.modeling.postprocessing import detector_postprocess
+from detectron2.structures import ImageList
+from detectron2.utils.logger import log_first_n
 from torch import nn
 
 from .heads import build_anchor_free_head
@@ -41,4 +46,62 @@ class AnchorFreeNetwork(nn.Module):
                 The :class:`Instances` object has the following keys:
                     "pred_boxes", "pred_classes", "scores", "pred_masks", "pred_keypoints"
         """
-        pass
+        if not self.training:
+            return self.inference(batched_inputs)
+
+        images = self.preprocess_image(batched_inputs)
+        if "instances" in batched_inputs[0]:
+            gt_instances = [x["instances"].to(self.device) for x in batched_inputs]
+        elif "targets" in batched_inputs[0]:
+            log_first_n(
+                logging.WARN, "'targets' in the model inputs is now renamed to 'instances'!", n=10
+            )
+            gt_instances = [x["targets"].to(self.device) for x in batched_inputs]
+        else:
+            gt_instances = None
+
+        features = self.backbone(images.tensor)
+
+        _, detector_losses = self.anchor_free_head(features, gt_instances)
+
+        losses = {}
+        losses.update(detector_losses)
+        return losses
+
+    def inference(self, batched_inputs, do_postprocess=True):
+        """
+        Run inference on the given inputs.
+        Args:
+            batched_inputs (list[dict]): same as in :meth:`forward`
+            do_postprocess (bool): whether to apply post-processing on the outputs.
+        Returns:
+            same as in :meth:`forward`.
+        """
+        assert not self.training
+
+        images = self.preprocess_image(batched_inputs)
+        features = self.backbone(images.tensor)
+
+        results, _ = self.anchor_free_head(features, None)
+
+        if do_postprocess:
+            processed_results = []
+            for results_per_image, input_per_image, image_size in zip(
+                    results, batched_inputs, images.image_sizes
+            ):
+                height = input_per_image.get("height", image_size[0])
+                width = input_per_image.get("width", image_size[1])
+                r = detector_postprocess(results_per_image, height, width)
+                processed_results.append({"instances": r})
+            return processed_results
+        else:
+            return results
+
+    def preprocess_image(self, batched_inputs):
+        """
+        Normalize, pad and batch the input images.
+        """
+        images = [x["image"].to(self.device) for x in batched_inputs]
+        images = [self.normalizer(x) for x in images]
+        images = ImageList.from_tensors(images, self.backbone.size_divisibility)
+        return images
--- anchor_free/config.py
+++ anchor_free/config.py
@@ -10,3 +10,9 @@ def add_anchor_free_config(cfg):
     _C.MODEL.ANCHOR_FREE_HEADS = CN()
 
     _C.MODEL.ANCHOR_FREE_HEADS.NAME = "RepHead"
+    # Number of foreground classes
+    _C.MODEL.ANCHOR_FREE_HEADS.NUM_CLASSES = 80
+    # Names of the input feature maps to be used by anchor free heads
+    # Currently all heads (box, mask, ...) use the same input feature map list
+    # e.g., ["p2", "p3", "p4", "p5"] is commonly used for FPN
+    _C.MODEL.ANCHOR_FREE_HEADS.IN_FEATURES = ["res4"]
