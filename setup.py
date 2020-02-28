31bfc7c2f7c588342052ffc7842926f9d01beae3
leftthomas <leftthomas@qq.com>
2020-02-28 16:10:53 +0800

    add carafe cuda

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+NVCC_ARGS = [
+    '-D__CUDA_NO_HALF_OPERATORS__',
+    '-D__CUDA_NO_HALF_CONVERSIONS__',
+    '-D__CUDA_NO_HALF2_OPERATORS__',
+]
+
+VERSION = '0.0.1'
+
+setup(
+    name='carafe',
+    version=VERSION,
+    license='MIT',
+    ext_modules=[
+        CUDAExtension(
+            'carafe_cuda',
+            ['src/carafe_cuda.cpp', 'src/carafe_cuda_kernel.cu'],
+            extra_compile_args={
+                'cxx': [],
+                'nvcc': NVCC_ARGS
+            }),
+        CUDAExtension(
+            'carafe_naive_cuda',
+            ['src/carafe_naive_cuda.cpp', 'src/carafe_naive_cuda_kernel.cu'],
+            extra_compile_args={
+                'cxx': [],
+                'nvcc': NVCC_ARGS
+            })
+    ],
+    cmdclass={'build_ext': BuildExtension},
+    install_requires=['torch'])
